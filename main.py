68ff953ab4dc7aae8dd99125c2f072e286a554cd
danik_tro <s.danik.tro.c@gmail.com>
2020-10-31 13:57:26 +0200

    Has been added files for push in container and blob

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, _
 connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 
 blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-container_name = "Trotsenko_Daniil" + str(uuid.uuid4())
+container_name = "trotsenkodaniil01"
 container_client = blob_service_client.create_container(container_name)
+
