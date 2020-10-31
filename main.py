e7e38d8278d35b4132a898e3506f217645411556
danik_tro <s.danik.tro.c@gmail.com>
2020-10-31 13:46:08 +0200

    Has been added function for connect_str

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import uuid
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
 
-try:
-    print("Azure Blob strorage v" + __version__ )
-except Exception as ex:
-    print("Exception:")
-    print(ex)
+connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+
+blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+container_name = "Trotsenko_Daniil" + str(uuid.uuid4())
+container_client = blob_service_client.create_container(container_name)
