052181555322deb444d28745e899aa3ab76b7a81
danik_tro <s.danik.tro.c@gmail.com>
2020-11-01 16:21:49 +0200

    Has been changed file

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
-import uuid
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
 connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 
 blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 container_name = "trotsenkodaniil01"
 container_client = blob_service_client.create_container(container_name)
-
