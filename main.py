712a614f2a1a20ebcfa434f76ce81c1ae096da0f
danik_tro <s.danik.tro.c@gmail.com>
2020-11-01 16:29:34 +0200

    Has been added adding the blobs in container

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
 connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 
-blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-container_name = "trotsenkodaniil01"
-container_client = blob_service_client.create_container(container_name)
+
+try:
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+    container_name = "trotsenkodaniil01"
+    container_client = blob_service_client.create_container(container_name)
+except Exception:
+    print("Container had been created already.")
+
+try:
+    file_name = "hashed_feature.csv"
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
+
+    with open(file_name, 'rb') as data:
+        blob_client.upload_blob(data)
+except Exception:
+    print("File had been uploaded already.")
+
