669e313db64a33ac4c9c36d18af9f82e55d4a804
danik_tro <s.danik.tro.c@gmail.com>
2020-11-04 13:53:57 +0200

    Created login

---
+++ Azure Data Studio/create_login.sql
@@ -0,0 +1,7 @@
+-- ==============================================================================================================================
+-- Create SQL Login template for Azure SQL Database, Azure SQL Data Warehouse Database and Azure Synapse SQL Analytics on-demand
+-- ==============================================================================================================================
+
+CREATE LOGIN d_trotsenko
+	WITH PASSWORD = '!dFg#0*^hgn^'
+GO
