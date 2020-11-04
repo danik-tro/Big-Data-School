dcb68882243956f9de2fb1c6a1157fb57b5bbd8e
danik_tro <s.danik.tro.c@gmail.com>
2020-11-04 23:36:51 +0200

    Added create file format

---
+++ Azure Data Studio/create_file_format.sql
@@ -0,0 +1,18 @@
+-- ==========================================================================
+-- Create external file format template for Azure SQL Data Warehouse Database
+-- ==========================================================================
+
+
+
+CREATE EXTERNAL FILE FORMAT trotsenko_format WITH
+(
+	FORMAT_TYPE = DELIMITEDTEXT,
+	FORMAT_OPTIONS
+	(
+		FIELD_TERMINATOR = N',',
+		STRING_DELIMITER = N'',
+		DATE_FORMAT = N'yyyy-MM-dd',
+		USE_TYPE_DEFAULT = False
+	)
+)
+GO
