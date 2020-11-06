f265f06ca106d3c4f1e8a07dd8092d78aee04351
danik_tro <s.danik.tro.c@gmail.com>
2020-11-06 14:31:46 +0200

    Added carcas of app

--- DataLakeGeneration2/data_lake_gen_2.py
+++ DataLakeGeneration2/data_lake_gen_2.py
@@ -1,3 +1,31 @@
+from azure.storage.filedatalake import DataLakeServiceClient
+import logging
+import os
+
+def logging_name_function(func):
+    def wrap(*args, **kwargs):
+        logging.info(f"{func.__name__} has been started")
+        func(*args)
+        logging.info(f"{func.__name__} has been finished")
+    return wrap
+
+
 class DataLakeG2:
-    def __init__(self):
-        pass
+    logging.basicConfig(format='%(levelname)s - %(asctime)s - %(message)s',
+                        datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO,
+                        filename='BDS.log',
+                        filemode='w')
+
+    def __init__(self, connection_string=os.getenv("AZURE_DT_2"),
+                 container_name_="container01",
+                 file_name_='IndianFoodDatasetCSV.csv'):
+        self.dt_service = DataLakeServiceClient.from_connection_string(connection_string)
+        self.file_system_name = container_name_
+        self.file_name = file_name_
+
+    def create_file_system(self):
+        """
+            Create file system(Container)
+        """
+
+        self.file_system_client = self.dt_service.create_file_system(file_system=self.file_system_name)
